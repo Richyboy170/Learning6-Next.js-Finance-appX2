776ccc517159fcf521cf2d48855751f4537986d4
Richyboy170 <richyboy170@gmail.com>
2024-06-09 06:23:07 +0700

    09 June 2024

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+export default function Page() {
+  const pathname = usePathname();
+  return (
+    <div>  
+    <p>Dashboard Page</p>
+    <Link href="/dashboard/customers"
+    className={clsx( "text-blue-600" , { "text-green-600" : pathname === "/dashboard" }, )}>
+      <p>customers page</p></Link>
+    <Link href="/dashboard/invoices"><p>invoices page</p></Link>
+    </div>
+  );
+}
