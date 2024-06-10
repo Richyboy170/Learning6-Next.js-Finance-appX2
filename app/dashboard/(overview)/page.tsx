d3b83c6e3a609694a9f94ffa28d32e739e31b497
Richyboy170 <richyboy170@gmail.com>
2024-06-10 07:48:31 +0700

    10 June 2024/1

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+
+ 
+export default async function Page() {
+  const pathname = usePathname();
+  const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+      <div>  
+      <p>Dashboard Page</p>
+      <Link href="/dashboard/customers"
+      className={clsx( "text-blue-600" , { "text-green-600" : pathname === "/dashboard" }, )}>
+        <p>customers page</p></Link>
+      <Link href="/dashboard/invoices"><p>invoices page</p></Link>
+      </div>
+    </main>
+  );
+}
